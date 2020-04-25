57bc61619e0f48d269aac2913cbf064da7b4bebd
Yogesh Pradhan <yogeshpradhan@Yogeshs-Air.lan1>
2020-04-25 12:59:34 -0400

    Updates

---
+++ precache-manifest.b4cb9e490a31a980cf1584d9d15f27ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd621bde5346fe611863b8a5f81179a",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "1efcd0ceb7a7eced7763",
+    "url": "/react-todo/static/css/main.d0b5d7c5.chunk.css"
+  },
+  {
+    "revision": "9408e9933ce0d698e3f7",
+    "url": "/react-todo/static/js/2.979e0aa1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.979e0aa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1efcd0ceb7a7eced7763",
+    "url": "/react-todo/static/js/main.ccb15221.chunk.js"
+  },
+  {
+    "revision": "eef45a0ad4c07a601d35",
+    "url": "/react-todo/static/js/runtime-main.b1189551.js"
+  }
+]);
